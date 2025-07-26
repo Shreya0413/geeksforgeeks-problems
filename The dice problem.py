64c9cbb4beca87e400a786c59e23da6c2b00a711
Shreya Das <shreyadas0413@gmail.com>
2025-07-26 23:29:53 +0530

    Create The dice problem.py

---
+++ The dice problem.py
@@ -0,0 +1,19 @@
+#User function Template for python3
+
+class Solution:
+    def oppositeFaceOfDice(self, n):
+    	   if n==1:
+    	        return(6)
+    	   elif n==2:
+    	        return(5)
+    	   elif n==3:
+    	        return(4)
+    	   elif n==4:
+    	        return(3)
+    	   elif n==5:
+    	        return(2)
+    	   elif n==6:
+    	        return(1)
+    	   else:
+    	       print("Invalid input")
+    	  
