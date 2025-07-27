ca0e5d5894a142aee05ebfeadbba8febd37b7b0b
Shreya Das <shreyadas0413@gmail.com>
2025-07-27 22:06:08 +0530

    Update Count Odd and Even.py

--- Count Odd and Even.py
+++ Count Odd and Even.py
@@ -2,8 +2,8 @@ class Solution:
     def countOddEven(self, arr):
         count_even = 0
         count_odd = 0
-        for num in arr:
-            if num % 2 == 0:
+        for num in arr: # to check each num of the array or this can be written like for num in range(len(arr)):
+            if num % 2 == 0: # if num[i]%2==0:
                 count_even += 1
             else:
                 count_odd += 1
