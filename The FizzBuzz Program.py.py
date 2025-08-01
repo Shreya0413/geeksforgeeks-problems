b81021cd65200c4c69dc926a4d761e75acc928fa
Shreya Das <shreyadas0413@gmail.com>
2025-08-02 03:38:01 +0530

    Create The FizzBuzz Program.py.py

---
+++ The FizzBuzz Program.py.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+number = int(input())
+if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+elif number % 3 == 0:
+    print("Fizz")
+elif number % 5 == 0:
+    print("Buzz")
+else:
+    print(number)
