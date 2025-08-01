c7d4499ed125994dc07a743ceb284ecac3f3e959
Shreya Das <shreyadas0413@gmail.com>
2025-08-02 03:49:19 +0530

    Create String Functions I - Python.py

---
+++ String Functions I - Python.py
@@ -0,0 +1,14 @@
+def trim(str):
+    return str.strip() # to trim use strip
+
+
+def exists(str, x):
+    return str.find(x)
+
+
+def titleIt(str):
+    return str.title()
+
+
+def casesSwap(str):
+    return str.swapcase()
